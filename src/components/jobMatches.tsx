114e7adbafb152668be536258a5df21c69e56c74
panisara-bd <panisara.bnn@gmail.com>
2023-08-01 13:59:44 +0200

    Set up useQuery and Axios

--- src/components/jobMatches.tsx
+++ src/components/jobMatches.tsx
@@ -1,6 +1,33 @@
 import { Button } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
-export default function JobMatches () {
-
-    return <><div>Hello! You can match developers to a job here.</div><Button variant="contained">Testing MUI and it works!</Button></>
+type JobData = {
+    id: string
+    description:  {
+        text: string
+    }
 }
+
+const hardCodedId = '27894030'
+
+const fetchJobById = async (id: string) => {
+    const res = await axios.get(`https://jobsearch.api.jobtechdev.se/ad/${id}`);
+    return res.data;
+}
+
+const JobMatches = (id: string) => {
+    const {
+        isLoading,
+        error,
+        data
+      } = useQuery<JobData, Error>( ['job'], () => fetchJobById(hardCodedId));
+
+      if (isLoading) return 'Loading...'
+
+      if (error) return 'An error has occurred: ' + error.message
+
+    return <><div>Hello! You can match developers to a job here. {data.id}</div><Button variant="contained">Testing MUI and it works!</Button></>
+}
+
+export default JobMatches;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Root from './routes/root.tsx';
 import AllJobsRoute from './routes/all-jobs.tsx';
 import JobMatchesRoute from './routes/job-matches.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -22,8 +23,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
