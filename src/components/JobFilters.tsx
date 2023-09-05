aafc65eae72c1a2714e05a634de5abb07791c879
panisara-bd <panisara.bnn@gmail.com>
2023-09-05 14:00:53 +0200

    Add autocomplete filer

--- src/components/JobFilters.tsx
+++ src/components/JobFilters.tsx
@@ -1,67 +1,83 @@
-import { Autocomplete, Checkbox, FormControlLabel, TextField } from '@mui/material';
+import {
+  Autocomplete,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from '@mui/material';
 import { Skill } from '../types/innerTypes';
 import { Dispatch, SetStateAction } from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 
 interface IJobFilters {
   setSearchKeyword: Dispatch<SetStateAction<FilterFormValues>>;
-  skills: Skill[]
+  skills: Skill[];
 }
 
 export type FilterFormValues = {
-  searchKeyword: string,
-  isRemote: boolean,
-  isExperienced?: boolean 
-}
+  searchKeyword: string;
+  isRemote: boolean;
+  isExperienced?: boolean;
+};
 
 const JobFilters = ({ setSearchKeyword, skills }: IJobFilters) => {
   const { register, handleSubmit } = useForm({
     defaultValues: {
-    searchKeyword: 'JavaScript',
-    isRemote: false,
-    isExperienced:  false
-    }
+      searchKeyword: 'JavaScript',
+      isRemote: false,
+      isExperienced: false,
+    },
   });
   // const [inputFilterValue, setInputFilterValue] = useState();
 
   const processingData = (data: FilterFormValues) => {
-    setSearchKeyword(data)
+    setSearchKeyword(data);
     console.log('hellew', data);
-    
-  }
+  };
 
   if (!skills) {
     return;
   }
 
   return (
-
     <form onSubmit={handleSubmit(processingData)}>
-      <FormControlLabel 
-      label='' 
-      control={<Autocomplete
-      {...register('searchKeyword')}
-        id="skill-selection"
-        freeSolo
-        sx={{background: '#ffffff'}}
-        options={skills.map((skill) => skill.title)}
-        renderInput={(params) => <TextField {...params} label="Search skills" />}
-      />} 
+      <Controller name="searchKeyword"
+      render={({field: {onChange, value}}) =>  <FormControlLabel
+        label=""
+        control={
+          <Autocomplete
+            id="skill-selection"
+            freeSolo
+            sx={{ background: '#ffffff' }}
+            options={skills.map((skill) => skill.title)}
+            value={value}
+            renderInput={(params) => (
+              <TextField {...params} label="Search skills" />
+            )}
+            onChange={onChange}
+          />
+        }
+      />}/>
+     
+
+      <FormControlLabel
+        {...register('isRemote')}
+        control={<Checkbox />}
+        label="isRemote"
       />
-      
-      <FormControlLabel {...register('isRemote')} control={<Checkbox />} label="isRemote" />
-      <FormControlLabel {...register('isExperienced')} control={<Checkbox />} label="isExperienced" />
-      <button type='submit'>Search</button>
+      <FormControlLabel
+        {...register('isExperienced')}
+        control={<Checkbox />}
+        label="isExperienced"
+      />
+      <button type="submit">Search</button>
     </form>
   );
 };
 
 export default JobFilters;
 
-
-// add filtering REMOTE 
+// add filtering REMOTE
 // add filtering EXPERIENCE
 
 // add filtering REGION
 // add filtering PROGRAMMING LANGUAGES
-
