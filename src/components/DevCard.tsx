edb72789c6c255b386b1d2943eb8e4e85b566279
Finns841594 <fengy142857@gmail.com>
2023-09-29 10:05:28 +0200

    fixed build error

--- src/components/DevCard.tsx
+++ src/components/DevCard.tsx
@@ -29,10 +29,13 @@ type Skill = {
   type: string;
 };
 
-const cardColorLogic = { JavaScript: '#ef9f76', 'C#': '#81c8be' };
+const cardColorLogic: { [key: string]: string } = {
+  JavaScript: '#ef9f76',
+  'C#': '#81c8be',
+};
 
 const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
+  const { ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
