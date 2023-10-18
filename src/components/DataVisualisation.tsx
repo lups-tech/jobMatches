1d0e2c77a569629e8492e7932b8dd885e6663611
Chris obrien <chrisobrien88@gmail.com>
2023-10-18 18:46:24 +0200

    user can add forms to compare multiple languages. Problem with state updating when creating a new form component and data set

--- src/components/DataVisualisation.tsx
+++ src/components/DataVisualisation.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { SearchResult } from "../types/externalTypes";
 import { useThemeContext } from "../theme";
 import { useState, useEffect } from "react";
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
 import { ChartExample } from "./ChartExample";
 
 const getDataBySearchAndDates = async (
@@ -20,101 +20,142 @@ const getDataBySearchAndDates = async (
   }
 };
 
-const DataVisualisation = () => {
-  const { darkMode } = useThemeContext();
+const labels = ["3-4 weeks ago", "2-3 weeks ago", "1-2 weeks ago", "Last week"];
 
-  const [searchKeyword, setSearchKeyword] = useState<string>("")
+const FormComponent = ({
+  jobsPerWeekData,
+  setJobsPerWeekData,
+  formId,
+}: any) => {
+  const todaysDate = new Date(Date.now()).toISOString().replace(/T.*/, "");
+  const oneMonth = 2592000000;
+  const oneMonthAgoDate = new Date(Date.now() - oneMonth)
+    .toISOString()
+    .replace(/T.*/, "");
+
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
 
   const [thisWeekCount, setThisWeekCount] = useState<number>(0);
   const [oneWeekOldCount, setOneWeekOldCount] = useState<number>(0);
   const [twoWeekOldCount, setTwoWeekOldCount] = useState<number>(0);
   const [threeWeekOldCount, setThreeWeekOldCount] = useState<number>(0);
-  const [isLoadingData, setIsLoadingData] = useState<boolean>(false)
 
-  const { isLoading, error, data } = useQuery<any>(["publicationDates", searchKeyword],  () =>
-    getDataBySearchAndDates(searchKeyword, "2023-09-10", "2023-10-18")
+  const { isLoading, error, data } = useQuery<any>(
+    ["publicationDates", searchKeyword],
+    () => getDataBySearchAndDates(searchKeyword, oneMonthAgoDate, todaysDate)
   );
-  
   const handleFormSubmit = (e: any) => {
     e.preventDefault();
-    setThisWeekCount(0)
-    setOneWeekOldCount(0)
-    setTwoWeekOldCount(0)
-    setThreeWeekOldCount(0)
+    setThisWeekCount(0);
+    setOneWeekOldCount(0);
+    setTwoWeekOldCount(0);
+    setThreeWeekOldCount(0);
     setSearchKeyword(e.target.searchTerm.value);
   };
 
-  useEffect(() => {
-    setIsLoadingData(true)
-      const dataPublicationData = data?.hits.map((job: any) => job.publication_date);
-      dataPublicationData?.forEach((date: any) => {
-        const timeDiff = Date.now() - new Date(date).getTime();
-        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-        if (days < 8) {
-          setThisWeekCount(prev => prev + 1);
-        }
-        if (days >= 8 && days < 15) {
-          setOneWeekOldCount(prev => prev + 1);
-        }
-        if (days >= 15 && days < 22) {
-          setTwoWeekOldCount(prev => prev + 1);
-        }
-        if (days >= 22 && days < 29) {
-          setThreeWeekOldCount(prev => prev +1)
-        }
-      });
-      setIsLoadingData(false)
-  }, [data, searchKeyword]);
-  
-
-  const counts: number[] = [threeWeekOldCount, twoWeekOldCount, oneWeekOldCount, thisWeekCount];
-  const counstsTwo: number[] = [5, 5, 20, 56]
-
-  const labels = [
-    "3-4 weeks ago",
-    "2-3 weeks ago",
-    "1-2 weeks ago",
-    "Last week",
+  const counts: number[] = [
+    threeWeekOldCount,
+    twoWeekOldCount,
+    oneWeekOldCount,
+    thisWeekCount,
   ];
 
-  const jobsPerWeekData = {
-    labels,
-    datasets: [
-      {
-        label: searchKeyword,
-        data: counts,
-        borderColor: "rgb(46, 73, 93)",
-        backgroundColor: "rgba(83, 173, 224, 0.5)",
-      },
-      {
-        label: "JavaScript",
-        data: counstsTwo,
-        borderColor: "rgb(55, 94, 80)",
-        backgroundColor: "rgba(103, 175, 161, 0.5)",
-      },
-    ],
-  };
+    //  const index = jobsPerWeekData.dataset.findIndex((dataset: any) => dataset.id === thisFormId)
 
-  if (isLoading || isLoadingData) return "Loading...";
+  useEffect(() => {
+    const dataPublicationData = data?.hits.map(
+      (job: any) => job.publication_date
+    );
+    dataPublicationData?.forEach((date: any) => {
+      const timeDiff = Date.now() - new Date(date).getTime();
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      if (days < 8) {
+        setThisWeekCount((prev) => prev + 1);
+      }
+      if (days >= 8 && days < 15) {
+        setOneWeekOldCount((prev) => prev + 1);
+      }
+      if (days >= 15 && days < 22) {
+        setTwoWeekOldCount((prev) => prev + 1);
+      }
+      if (days >= 22 && days < 29) {
+        setThreeWeekOldCount((prev) => prev + 1);
+      }
+    });
+  }, [data, searchKeyword]);
+
+  if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: " + error;
 
   return (
+    <form className="m-10" onSubmit={handleFormSubmit} name="searchTerm1">
+      <input
+        type="text"
+        name="searchTerm"
+        placeholder="Enter search term"
+        list="programmingLanguages"
+      />
+      <datalist id="programmingLanguages">
+        <option value="javascript" />
+        <option value="java" />
+        <option value="python" />
+      </datalist>
+      <Button type="submit">Search</Button>
+    </form>
+  );
+};
+
+const DataVisualisation = () => {
+  const [jobsPerWeekData, setJobsPerWeekData] = useState<any>({
+    labels,
+    datasets: [
+    ],
+  });
+
+  const { darkMode } = useThemeContext();
+
+  const [formArray, setFormArray] = useState<any[]>([]);
+
+  const addNewFormAndDataset = () => {
+    const formId = Date.now();
+  
+    setJobsPerWeekData((prevState: any) => {
+      const newDataset = {
+        id: formId,
+        label: "",
+        data: [0, 0, 0, 0],
+        borderColor: "rgb(229, 217, 217)",
+        backgroundColor: "rgba(20, 144, 216, 0.5)",
+      };
+  
+      // Create a new array with the existing datasets and the new dataset
+      const newDatasets = [...prevState.datasets, newDataset];
+  
+      // Return a new state with the updated datasets
+      return {
+        ...prevState,
+        datasets: newDatasets,
+      };
+    });
+
+  
+    setFormArray([
+      ...formArray,
+      <FormComponent
+        jobsPerWeekData={jobsPerWeekData}
+        setJobsPerWeekData={setJobsPerWeekData}
+        formId={formId}
+      />,
+    ]);
+  };
+
+  return (
     <div className={`pt-10  pb-96 ${darkMode ? "bg-[#97B2EF]" : "bg-Blue"}`}>
-      <form className="m-10" onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          name="searchTerm"
-          placeholder="Enter search term"
-          list="programmingLanguages"
-        />
-        <datalist id="programmingLanguages">
-          <option value="javascript" />
-          <option value="java" />
-          <option value="python" />
-        </datalist>
-        <Button type="submit">Search</Button>
-      </form>
+      {formArray.map((form: any, index) => {
+        return <div key={index}>{form}</div>;
+      })}
+      <Button onClick={addNewFormAndDataset}>Add Language</Button>
       <div className="grid md:grid-cols-3 grid-flow-row gap-3 text-white w-4/5 m-10">
         <ChartExample jobsPerWeekData={jobsPerWeekData} />
       </div>
