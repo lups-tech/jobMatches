2a806a7c8c411735e65d2cc2ef04c1b4b0be8a5e
Finns841594 <fengy142857@gmail.com>
2023-10-19 11:53:25 +0200

    fix broswer warning

--- src/components/SavedDevsList.tsx
+++ src/components/SavedDevsList.tsx
@@ -4,7 +4,7 @@ import DevCard from './DevCard';
 
 const SavedDevsList = ({ developers }: { developers: DeveloperDTO[] }) => {
   return (
-    <div className="w-[415px] mx-5" overflow-hidden hover:overflow-y-auto>
+    <div className="w-[415px] mx-5">
       <Typography variant="h5">Saved Developers</Typography>
       <div className="w-full h-[600px] mx-auto overflow-hidden hover:overflow-y-auto">
         <div className="w-[400px]">
