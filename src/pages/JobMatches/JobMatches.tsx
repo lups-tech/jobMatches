6a3c78fd68e74e231ced0ede6ce60dbfbee33d73
SMooreSwe <stevedm88@gmail.com>
2023-11-14 13:41:54 +0100

    added call to action in jobMatches page

--- src/pages/JobMatches/JobMatches.tsx
+++ src/pages/JobMatches/JobMatches.tsx
@@ -14,6 +14,7 @@ import { findMatchingSkills, sortMockDevelopers } from '../../utils/utilities';
 import JobMatchesJobPaper from './components/JobMatchesJobPaper';
 import JobMatchesDevPaper from './components/JobMatchesDevPaper';
 import { MatchingProcess, UserInfoDTO } from '../../types/innerTypes';
+import CallToAction from './components/CallToAction';
 
 type LocationState = {
   state: Job;
@@ -134,6 +135,7 @@ export const JobMatches = () => {
               <Typography variant="h5">No good matches</Typography>
             </Paper>
           )}
+          {!isAuthenticated && <CallToAction/>}
         </div>
       </div>
     </div>
