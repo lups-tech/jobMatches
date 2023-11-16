2049ef48172d78c90016a4af8625e0e7fadea22a
SMooreSwe <stevedm88@gmail.com>
2023-11-16 13:19:12 +0100

    added clearer styling to talenthub area of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Button, Toolbar, useMediaQuery } from '@mui/material';
+import { AppBar, Toolbar, useMediaQuery } from '@mui/material';
 import HubIcon from '@mui/icons-material/Hub';
 import Groups2Icon from '@mui/icons-material/Groups2';
 import WorkIcon from '@mui/icons-material/Work';
@@ -24,17 +24,19 @@ export const Navbar = () => {
     >
       <Toolbar>
         <div className="w-full grid grid-cols-3 h-full">
-          <div className="flex flex-row justify-start align-middle items-center pt-2">
-            <Link to="/" color="#ffffff">
-              <Button variant='text' sx={{marginLeft: '-1rem'}}>
-                <HubIcon sx={{marginRight: '0.3rem'}}/>
+          <div className="flex flex-row justify-start items-center pt-6">
+            <Link to="/" color="#ffffff" className={`border-b-[4px] h-full ${
+                    isAuthenticated && location.pathname === '/'
+                      ? 'border-slate-100 font-bold'
+                      : 'border-transparent'
+                  }`}>
+                <HubIcon sx={{marginBottom: "0.1rem", marginRight: '0.3rem'}}/>
                 TalentHub
-              </Button>
             </Link>
           </div>
           <div className="flex flex-row gap-15 justify-center items-center h-full">
             {isAuthenticated && (
-              <div className={`border-b-[4px] h-full pt-4
+              <div className={`border-b-[4px] h-full pt-6 mx-4
               ${
                 location.pathname === '/developers'
                   ? 'border-slate-100 font-bold'
@@ -44,14 +46,12 @@ export const Navbar = () => {
                   to="/developers"
                   color="#ffffff"
                 >
-                  <Button variant='text'>
-                    <Groups2Icon sx={{marginRight: '0.3rem'}}/>
+                    <Groups2Icon sx={{marginBottom: "0.1rem", marginRight: '0.3rem'}}/>
                     Developers
-                  </Button>
                 </Link>
               </div>
             )}
-            <div className={`border-b-[4px] h-full pt-4
+            <div className={`border-b-[4px] h-full pt-6 mx-4
                   ${
                     location.pathname === '/jobs'
                       ? 'border-slate-100 font-bold'
@@ -60,12 +60,9 @@ export const Navbar = () => {
               <Link
                 to="/jobs"
                 color="#ffffff"
-                
               >
-                <Button variant="text">
-                  <WorkIcon sx={{marginRight: '0.3rem'}}/>
+                  <WorkIcon sx={{marginBottom: "0.1rem", marginRight: '0.3rem'}}/>
                   Jobs
-                </Button>
               </Link>
             </div>
             </div>
