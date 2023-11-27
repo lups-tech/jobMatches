e6fdcb2266d30283b1afa527c902824cf03f798d
SMooreSwe <stevedm88@gmail.com>
2023-11-27 11:40:36 +0100

    removed random avatars and added backup colour logic

--- src/data/programmingLanguageColors.ts
+++ src/data/programmingLanguageColors.ts
@@ -5,3 +5,11 @@ export const cardColorLogic: { [key: string]: string } = {
   Java: '#e78284',
   no_such_programming_skill: '#fff',
 };
+
+export const avatarColorLogic: { [key: string]: string } = {
+  JavaScript: '#90ADE5',
+  'C#': '#C8818B',
+  Python: '#BEEEEE',
+  Java: '#828282',
+  no_such_programming_skill: '#000000',
+};
